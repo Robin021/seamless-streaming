1727d3bc7b67086943704192f17e333c6a6e1e9f
Mark Duppenthaler <mduppes@gmail.com>
2023-09-06 10:05:59 -0700

    work with m4t model

--- app.py
+++ app.py
@@ -11,73 +11,141 @@ from seamless_communication.models.inference.translator import Translator
 
 from m4t_app import *
 
-from transformers import pipeline
-
-p = pipeline("automatic-speech-recognition")
-
 from pydub import AudioSegment
 import time
 from time import sleep
 
-m4t_demo()
+# m4t_demo()
 
-def transcribe(audio, state=""):
-    # sleep(2)
-    print('state', state)
-    text = p(audio)["text"]
-    state += text + " "
-    return state
+USE_M4T = True
+
+
+def translate_audio_file_segment(audio_file):
+    print("translate_m4t state")
+
+    return predict(
+        task_name="S2ST",
+        audio_source="microphone",
+        input_audio_mic=audio_file,
+        input_audio_file=None,
+        input_text="",
+        source_language="English",
+        target_language="Portuguese",
+    )
+
+
+def translate_m4t_callback(
+    audio_file, translated_audio_bytes_state, translated_text_state
+):
+    translated_wav_segment, translated_text = translate_audio_file_segment(audio_file)
+    print('translated_audio_bytes_state', translated_audio_bytes_state)
+    print('translated_wav_segment', translated_wav_segment)
+
+    # combine translated wav into larger..
+    if type(translated_audio_bytes_state) is not tuple:
+        translated_audio_bytes_state = translated_wav_segment
+    else:
+
+        translated_audio_bytes_state = (translated_audio_bytes_state[0], np.append(translated_audio_bytes_state[1], translated_wav_segment[1]))
+
+    # translated_wav_segment[1]
+
+
+    translated_text_state += " | " + str(translated_text)
+    return [
+        audio_file,
+        translated_wav_segment,
+        translated_audio_bytes_state,
+        translated_text_state,
+        translated_audio_bytes_state,
+        translated_text_state,
+    ]
+
+
+def clear():
+    print("Clearing State")
+    return [bytes(), ""]
+
 
 def blocks():
     with gr.Blocks() as demo:
-        total_audio_bytes_state = gr.State(bytes())
-        total_text_state = gr.State("")
+        translated_audio_bytes_state = gr.State(None)
+        translated_text_state = gr.State("")
 
         # input_audio = gr.Audio(label="Input Audio", type="filepath", format="mp3")
-        input_audio = gr.Audio(label="Input Audio", type="filepath", format="mp3", source="microphone", streaming=True)
-        with gr.Row():
-            with gr.Column():
-                stream_as_bytes_btn = gr.Button("Stream as Bytes")
-                stream_as_bytes_output = gr.Audio(format="bytes", streaming=True)
-                stream_output_text = gr.Textbox(label="Translated text")
+        if USE_M4T:
+            input_audio = gr.Audio(
+                label="Input Audio",
+                type="filepath",
+                source="microphone",
+                streaming=True,
+            )
+        else:
+            input_audio = gr.Audio(
+                label="Input Audio",
+                type="filepath",
+                format="mp3",
+                source="microphone",
+                streaming=True,
+            )
 
+        most_recent_input_audio_segment = gr.Audio(
+            label="Recent Input Audio Segment segments", format="bytes", streaming=True
+        )
+        # TODO: Should add combined input audio segments...
 
-                def stream_bytes(audio_file, total_audio_bytes_state, total_text_state):
-                    chunk_size = 30000
+        stream_as_bytes_btn = gr.Button("Translate most recent recording segment")
 
-                    print(f"audio_file {audio_file}, size {os.path.getsize(audio_file)}")
-                    with open(audio_file, "rb") as f:
+        output_translation_segment = gr.Audio(
+            label="Translated audio segment",
+            autoplay=False,
+            streaming=True,
+            type="numpy",
+        )
 
-                        while True:
-                            chunk = f.read(chunk_size)
-                            if chunk:
-                                total_audio_bytes_state += chunk
-                                print('yielding chunk', len(chunk))
-                                print('total audio bytes', len(total_audio_bytes_state))
-                                print(f"Text state: {total_text_state}")
+        output_translation_combined = gr.Audio(
+            label="Translated audio combined",
+            autoplay=False,
+            streaming=True,
+            type="numpy",
+        )
 
-                                # This does the whole thing every time
-                                # total_text = transcribe(chunk, "")
-                                # yield total_audio_bytes_state, total_text, total_audio_bytes_state, total_text_state
+        # Could add output text segment
+        stream_output_text = gr.Textbox(label="Translated text")
 
-                                # This translates just the new part every time
-                                total_text_state = transcribe(chunk, total_text_state)
-                                total_text = total_text_state
-                                # total_text = transcribe(chunk, total_text)
-                                yield total_audio_bytes_state, total_text, total_audio_bytes_state, total_text_state
-                                # sleep(3)
-                            else:
-                                break
-                def clear():
-                    print('clearing')
-                    return [bytes(), ""]
+        stream_as_bytes_btn.click(
+            translate_m4t_callback,
+            [input_audio, translated_audio_bytes_state, translated_text_state],
+            [
+                most_recent_input_audio_segment,
+                output_translation_segment,
+                output_translation_combined,
+                stream_output_text,
+                translated_audio_bytes_state,
+                translated_text_state,
+            ],
+        )
 
-                stream_as_bytes_btn.click(stream_bytes, [input_audio, total_audio_bytes_state, total_text_state], [stream_as_bytes_output, stream_output_text, total_audio_bytes_state, total_text_state])
-
-        input_audio.change(stream_bytes, [input_audio, total_audio_bytes_state, total_text_state], [stream_as_bytes_output, stream_output_text, total_audio_bytes_state, total_text_state])
-        input_audio.clear(clear, None, [total_audio_bytes_state, total_text_state])
-        input_audio.start_recording(clear, None, [total_audio_bytes_state, total_text_state])
-
+        input_audio.change(
+            translate_m4t_callback,
+            [input_audio, translated_audio_bytes_state, translated_text_state],
+            [
+                most_recent_input_audio_segment,
+                output_translation_segment,
+                output_translation_combined,
+                stream_output_text,
+                translated_audio_bytes_state,
+                translated_text_state,
+            ],
+        )
+        # input_audio.change(stream_bytes, [input_audio, translated_audio_bytes_state, translated_text_state], [most_recent_input_audio_segment, stream_output_text, translated_audio_bytes_state, translated_text_state])
+        # input_audio.change(lambda input_audio: recorded_audio, [input_audio], [recorded_audio])
+        input_audio.clear(
+            clear, None, [translated_audio_bytes_state, translated_text_state]
+        )
+        input_audio.start_recording(
+            clear, None, [translated_audio_bytes_state, translated_text_state]
+        )
 
     demo.queue().launch()
 
